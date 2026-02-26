6072e060f197728f0d4eefc7addcba2bc14f60d9
carevaplus-jpg <carevaplus@gmail.com>
2026-02-26 20:15:13 +0530

    Wrap CarevaPlusRoyalAI in a styled div with header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import React from "react";
 import CarevaPlusRoyalAI from "./CarevaPlusRoyalAI";
 
 export default function App() {
-  return <CarevaPlusRoyalAI />;
+  return (
+    <div style={{padding:"20px"}}>
+      <h1>👑 Careva+ AI CRM</h1>
+      <CarevaPlusRoyalAI />
+    </div>
+  );
 }
